7c674ec2978f4527c1628697ea20d695a17a3c9e
Moritz Manner <moritz.manner@gmail.com>
2017-10-24 15:01:20 +0200

    added routes for new_author (closes #1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'authors/new'#, to: 'authors#new'
+  get 'new_author', to: 'authors#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
- 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
