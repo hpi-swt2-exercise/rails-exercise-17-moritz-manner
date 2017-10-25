a84952e6312f0496661eeafc7a22a54b245df951
Moritz Manner <moritz.manner@gmail.com>
2017-10-25 14:54:20 +0200

    controller saves and shows authors after submitting

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,22 @@
 class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+  end
+  def show
+    @author = Author.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:author].inspect
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to @author
   end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
