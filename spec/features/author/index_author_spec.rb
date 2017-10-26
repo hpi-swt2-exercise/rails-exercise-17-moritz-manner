f675ac1af4c0aec8add568c5662b3241ce559343
Moritz Manner <moritz.manner@gmail.com>
2017-10-26 19:41:14 +0200

    failing test for #11

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 describe "author index page", type: :feature do
  it "should display name of all authors" do
-   visit authors_path
+  visit authors_path
   expect(page).to have_css('th', :text => 'Name')
  end
+
+ it "should show the name and homepage of an author" do
+   author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+   author.save
+   visit authors_path
+   expect(page).to have_css('td', :text => 'http://wikipedia.org/Alan_Turing')
+  end
 end
