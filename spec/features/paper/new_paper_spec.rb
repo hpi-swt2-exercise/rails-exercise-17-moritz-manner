aa117db7bbad2e8c638ce67a19b3b6c80693caaa
Moritz Manner <moritz.manner@gmail.com>
2017-11-04 16:11:10 +0100

    Failing test for #21

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -5,4 +5,9 @@ describe 'New Paper page', type: :feature  do
   it "should render without error" do
     visit new_paper_path
   end
+
+  it "should have a title field" do
+    visit new_paper_path
+    expect(page).to have_field('Title')
+  end
 end
