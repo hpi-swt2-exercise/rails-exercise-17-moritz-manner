3159246d2aae913a6ffb676d957832e926420e0e
Moritz Manner <moritz.manner@gmail.com>
2017-11-04 16:15:00 +0100

    updated test for #21

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -6,8 +6,10 @@ describe 'New Paper page', type: :feature  do
     visit new_paper_path
   end
 
-  it "should have a title field" do
+  it "should have a title, venue and year field" do
     visit new_paper_path
     expect(page).to have_field('Title')
+    expect(page).to have_field('Venue')
+    expect(page).to have_field('Year')
   end
 end
