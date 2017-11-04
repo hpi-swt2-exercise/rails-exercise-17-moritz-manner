9a4cf59dd8aea66db0446f803b8f1f3aca875d9e
Moritz Manner <moritz.manner@gmail.com>
2017-11-04 17:18:45 +0100

    Failing test for #25

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Paper, type: :model do
     expect(paper.venue).to eq('Mind 49: 433-460')
     expect(paper.year).to eq(1950)
   end
+
+  it "must have a title" do
+    paper = Paper.new
+    paper.title = ''
+    expect(paper).to_not be_valid
+  end
 end
