342589aa63c9cd2a544f593117f29312612cc889
Moritz Manner <moritz.manner@gmail.com>
2017-11-04 14:27:57 +0100

    Failing test for #18

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -6,4 +6,15 @@ describe "Edit author page", type: :feature do
      author = FactoryGirl.create :author
      visit edit_author_path(author)
    end
+
+   it "should allow updating author information" do
+     author = FactoryGirl.create :author
+     expect(author.first_name).to_not eq('Alan Mathison')
+
+     visit edit_author_path(author)
+     fill_in "author_first_name", :with => 'Alan Mathison'
+     find('input[type="submit"]').click
+     author.reload
+     expect(author.first_name).to eq('Alan Mathison')
+   end
  end
