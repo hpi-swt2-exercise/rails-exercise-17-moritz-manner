303ad7ce1eada641268dc4527f2495ab96ef8548
Moritz Manner <moritz.manner@gmail.com>
2017-11-07 13:44:59 +0100

    Failing test for #31

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 describe "Show author page", type: :feature do
-  it "should display a paper's title" do
+  it "should display a paper's title, venue and year" do
     paper = FactoryGirl.create :paper
-    paper.save()
+
     visit paper_path(paper)
     expect(page).to have_content('COMPUTING MACHINERY AND INTELLIGENCE')
+    expect(page).to have_content('Mind 49: 433-460')
+        expect(page).to have_content(1950)
   end
 end
