2d8808a3a1f5010c8fbbaa721a14adfe146c5928
Moritz Manner <moritz.manner@gmail.com>
2017-11-07 14:07:10 +0100

    Failing test for #34

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -13,6 +13,12 @@ describe 'Paper index page', type: :feature do
     expect(page).to have_css('td', :text => 'COMPUTING MACHINERY AND INTELLIGENCE')
     expect(page).to have_css('td', :text => 'Mind 49: 433-460')
     expect(page).to have_css('td', :text => 1950)
+  end
 
+  it "should link to each paper page" do
+    paper = FactoryGirl.create :paper
+    paper.save
+    visit papers_path
+    expect(page).to have_css('a', :text => 'Show')
   end
 end
