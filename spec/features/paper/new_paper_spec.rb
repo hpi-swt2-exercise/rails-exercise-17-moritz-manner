e02fca64a15b6d899920f992cc9326aec49bfa11
Moritz Manner <moritz.manner@gmail.com>
2017-11-07 13:23:10 +0100

    Failing test for #29

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -18,11 +18,11 @@ describe 'New Paper page', type: :feature  do
     expect(page).to have_css('input[type="submit"]')
   end
 
-  # it "should create an instance of paper" do
-  #   visit new_paper_path
-  #   fill_in "paper_title", :with => 'COMPUTING MACHINERY AND INTELLIGENCE'
-  #   fill_in "paper_venue", :with => 'Mind 49: 433-460'
-  #   fill_in "paper_year", :with => '1950'
-  #   find('input[type="submit"]').click
-  # end
+  it "should create an instance of paper" do
+    visit new_paper_path
+    fill_in "paper_title", :with => 'COMPUTING MACHINERY AND INTELLIGENCE'
+    fill_in "paper_venue", :with => 'Mind 49: 433-460'
+    fill_in "paper_year", :with => '1950'
+    find('input[type="submit"]').click
+  end
 end
