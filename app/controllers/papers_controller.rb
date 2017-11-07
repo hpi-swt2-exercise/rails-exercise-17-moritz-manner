9d43e42697e26287f5bd53b937378aef4dfe3691
Moritz Manner <moritz.manner@gmail.com>
2017-11-07 13:23:34 +0100

    Added show and create to paper controller. Closes #29

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,6 +1,23 @@
 class PapersController < ApplicationController
+  def show
+    @paper = Paper.find(params[:id])
+  end
+
   def new
   end
+
   def create
+    @paper = Paper.new(paper_params)
+    if @paper.save()
+      redirect_to @paper
+    else
+      render 'new'
+    end
+
   end
+
+  private
+    def paper_params
+      params.require(:paper).permit(:title, :venue, :year)
+    end
 end
