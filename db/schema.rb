8e7d95e4b75117ea230b4619b2e3a2e6a85e6556
Moritz Manner <moritz.manner@gmail.com>
2017-11-08 14:45:33 +0100

    Added many to many relation. Closes #39

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104152404) do
+ActiveRecord::Schema.define(version: 20171108132305) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20171104152404) do
     t.text     "homepage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authors_papers", id: false, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "paper_id",  null: false
   end
 
   create_table "papers", force: :cascade do |t|
