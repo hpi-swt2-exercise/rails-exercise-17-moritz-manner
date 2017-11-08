7763642a43b9107ad76227883310d1614eadcaaa
Moritz Manner <moritz.manner@gmail.com>
2017-11-08 14:45:17 +0100

    Failing test for #39

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -28,4 +28,8 @@ RSpec.describe Paper, type: :model do
     paper.year = 'nineteen-fifty'
     expect(paper).to_not be_valid
   end
+  it "should have an empty list of authors" do
+  	paper = Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460', year: 1950)
+  	expect(paper.authors).to be_empty
+  	end
 end
