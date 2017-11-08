4be4d59086beaaa88874e9111787ee3729c30921
Moritz Manner <moritz.manner@gmail.com>
2017-11-08 14:07:16 +0100

    Failing test for #37

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -6,4 +6,14 @@ describe "Edit paper page", type: :feature do
     visit edit_paper_path(paper)
   end
 
+  it "should allow updating author information" do
+    paper = FactoryGirl.create :paper
+    expect(paper.venue).to_not eq('Mind 49')
+
+    visit edit_paper_path(paper)
+    fill_in "paper_venue", :with => 'Mind 49'
+    find('input[type="submit"]').click
+    paper.reload
+    expect(paper.venue).to eq('Mind 49')
+  end
 end
