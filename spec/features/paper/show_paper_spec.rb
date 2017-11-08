35443cdbb4af4c9cb32b84f18933ba1bee0156cb
Moritz Manner <moritz.manner@gmail.com>
2017-11-08 15:03:09 +0100

    Failing test for #40

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -9,4 +9,12 @@ describe "Show author page", type: :feature do
     expect(page).to have_content('Mind 49: 433-460')
         expect(page).to have_content(1950)
   end
+  it "should show the paper's author" do
+    paper = FactoryGirl.create :paper
+    author = FactoryGirl.create :author
+
+    paper.authors << author
+    visit paper_path(paper)
+    expect(page).to have_content(paper.authors.first.name)
+  end
 end
